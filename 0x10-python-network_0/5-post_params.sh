cb24081e20ee45c6569bd6b47262e85bd6263b92
babsy080 <musaabdullahi0398@gmail.com>
2023-03-30 08:06:46 -0700

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a POST request to the URL passed and displays body of response
-curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# Script sends POST to URL
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
