84b7fd695946bf98f139b851734a02a6fdc95b0a
babsy080 <musaabdullahi0398@gmail.com>
2023-03-31 08:49:09 -0700

    correction

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from urllib import request
-""" Script that reads from a URL """
+"Module that fetches https://alx-intranet.hbtn.io/status"
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as resp:
-        data = resp.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(data)))
-    print("\t- content: {}".format(data))
-    print("\t- utf8 content: {}".format(data.decode("utf-8")))
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
-from urllib.parse import urlencode
-from urllib.error import HTTPError
-from sys import argv
 """
-Script that sends a request to the URL passed in and displays
-the response
+script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
+You have to manage urllib.error.HTTPError exceptions and
+print: Error code: followed by the HTTP status code.
 """
+import urllib.request
+import urllib.error
+import sys
 
 
 if __name__ == "__main__":
+    url = sys.argv[1]
     try:
-        with urlopen(argv[1]) as resp:
-            print(resp.read().decode("utf-8"))
-    except HTTPError as error:
-            print("Error code: {}".format(error.code))
+        with urllib.request.urlopen(url) as response:
+            response_text = response.read().decode("utf-8")
+            print(response_text)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
